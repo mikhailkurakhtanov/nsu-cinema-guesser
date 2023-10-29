798a5d26c231a8c2ca9d08fd59022ac769c8ac56
Курахтанов Михаил Сергеевич <mikhail@csfullstack.com>
2023-10-29 09:27:03 +0700

    removed unused core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {CoreModule} from '@core/core.module';
 import {SharedModule} from '@shared/shared.module';
 import {ViewsModule} from '@views/views.module';
 
@@ -11,7 +10,7 @@ import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, SharedModule, ViewsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, ViewsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
