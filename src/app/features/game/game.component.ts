d72c01de8204bae5b0c0e9175891d39a088f1d6e
Mikhail Kurakhtanov <mikhail@csfullstack.com>
2023-12-14 16:39:35 +0700

    development

--- src/app/features/game/game.component.ts
+++ src/app/features/game/game.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Self} from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {ActivatedRoute, Router} from '@angular/router';
-import {filter, map, switchMap, take, takeUntil} from 'rxjs';
+import {filter, map, switchMap, take, takeUntil, tap} from 'rxjs';
 
 import {NgOnDestroy} from '@core/services/ng-on-destroy.service';
 import {GameParameterType, GameRound} from '@features/game/models/game.model';
@@ -62,9 +62,18 @@ export class GameComponent implements OnInit {
       .getParameter(this.roundInfo.id, type)
       .pipe(takeUntil(this.destroy))
       .subscribe(param => {
-        this.roundInfo.score = param.score;
+        if (!param.score) {
+          this.router
+            .navigate([''])
+            .then(() => this.snackBar.open('Игра закончена', undefined, {duration: constants.defaultSnackBarDuration}));
+        }
 
-        if (param.parameter.length) this.receivedTextParams.push(`${localizedParam}: ${param.parameter.join(', ')}`);
+        this.roundInfo.score = param.score;
+        if (param.parameter.length) {
+          this.receivedTextParams.push(`${localizedParam}: ${param.parameter.join(', ')}`);
+        } else {
+          this.snackBar.open('Не удалось получить подсказку', undefined, {duration: constants.defaultSnackBarDuration});
+        }
       });
   }
 
@@ -74,7 +83,7 @@ export class GameComponent implements OnInit {
       .pipe(
         map(roundInfo => (this.roundInfo = roundInfo)),
         filter(() => !!this.roundInfo),
-        switchMap(() => this.gameService.getAvailableParameters(Number(this.roundInfo?.id))),
+        switchMap(() => this.gameService.getAvailableParameters(Number(this.roundInfo.id))),
         map(params => (this.availableParams = params)),
         filter(() => !!this.availableParams.find(param => param === GameParameterType.IMAGES)),
         switchMap(() => this.gameService.getParameter(this.roundInfo.id, GameParameterType.IMAGES)),
@@ -90,25 +99,50 @@ export class GameComponent implements OnInit {
 
   confirmAnswer(): void {
     if (!this.roundInfo || !this.selectedAnswer.length) {
+      this.snackBar.open('Выберите вариант ответа', undefined, {duration: constants.defaultSnackBarDuration});
       return;
     }
 
+    this.gameIsLoaded = false;
+
     this.gameService
       .setAnswer(this.roundInfo.id, this.selectedAnswer)
-      .pipe(takeUntil(this.destroy))
-      .subscribe(result => {
-        this.currentRound++;
-        this.selectedAnswer = '';
+      .pipe(
+        map(result => {
+          if (result.right) {
+            this.receivedTextParams = [];
+            this.roundInfo = result.round;
 
-        this.roundInfo = result.round;
+            this.snackBar.open('Вы угадали!', undefined, {duration: constants.defaultSnackBarDuration});
+          } else {
+            this.gameIsLoaded = true;
+            this.snackBar.open('Неверный ответ. Попробуйте еще раз!', undefined, {duration: constants.defaultSnackBarDuration});
+          }
 
-        if (result.right) {
-          this.snackBar.open('Вы угадали!', undefined, {duration: constants.defaultSnackBarDuration});
-        }
+          if (!result.alive) {
+            this.router
+              .navigate([''])
+              .then(() => this.snackBar.open('Игра закончена.', undefined, {duration: constants.defaultSnackBarDuration}));
+          }
 
-        if (!result.alive) {
-          this.snackBar.open('Игра закончена', undefined, {duration: constants.defaultSnackBarDuration});
-        }
-      });
+          return result.alive && result.right;
+        }),
+        filter(refreshRoundData => refreshRoundData),
+        switchMap(() => this.gameService.getAvailableParameters(Number(this.roundInfo.id))),
+        map(params => (this.availableParams = params)),
+        filter(() => !!this.availableParams.find(param => param === GameParameterType.IMAGES)),
+        switchMap(() => this.gameService.getParameter(this.roundInfo.id, GameParameterType.IMAGES)),
+        map(param => {
+          this.roundInfo.score = param.score;
+          this.imageSrc = param.parameter[0];
+        }),
+        tap(() => {
+          this.currentRound++;
+          this.selectedAnswer = '';
+          this.gameIsLoaded = true;
+        }),
+        takeUntil(this.destroy),
+      )
+      .subscribe();
   }
 }
